eff52f829ae491b77ad08b930f3ffb6c8b787aa9
ETombuyses <e.tombuyses@gmail.com>
2020-06-11 10:57:35 +0200

    scss

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 
 function App() {
