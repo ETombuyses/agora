047392564a9707823209d04454ea97781f5cba25
ETombuyses <e.tombuyses@gmail.com>
2020-06-23 10:11:35 +0200

    [scss] configuration - variables, fonts, mixins, reset

--- src/App.js
+++ src/App.js
@@ -3,26 +3,14 @@ import React from 'react';
 // router
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import logo from './logo.svg';
-import './App.scss';
+import './scss/styles.scss';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <p>test font</p>
       </header>
     </div>
   );
