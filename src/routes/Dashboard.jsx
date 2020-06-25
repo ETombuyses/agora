f03c94a3da8e9db41f66307e226279bea1392e02
ETombuyses <e.tombuyses@gmail.com>
2020-06-25 16:03:45 +0200

    [component] WelcomeBanner component creation - mobile version

--- src/routes/Dashboard.jsx
+++ src/routes/Dashboard.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { LevelProgress } from '../components/molecules/LevelProgress'
+
+import { WelcomeBanner } from '../components/molecules/WelcomeBanner'
 import { Task } from '../components/molecules/Task'
+import { LevelProgress } from '../components/molecules/LevelProgress'
 
 /* -----------------------------------------------------COMPONENT------------------------------------------------ */
 
 export default function Dashboard() {
   return (
     <div className="pageWrapper">
+      <WelcomeBanner />
       <Task task="trash" progression={100}></Task>
       <Task task="water" progression={0}></Task>
       <Task task="gas" progression={49}></Task>
