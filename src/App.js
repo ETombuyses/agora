f88c3e696d513c37f88ebfc8fa22c8db99e0ff3f
LimongiVirgil <limongi.virgil@gmail.com>
2020-07-07 18:34:13 +0200

    [disclaimer] change size ans systeme of disclaimer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React, { useEffect, useRef } from 'react'
-import styled from 'styled-components'
+import React, { useEffect } from 'react'
 import { HashRouter as Router } from 'react-router-dom'
 
 // tools
@@ -7,7 +6,6 @@ import { getNewTokens } from './tools/isAuth'
 
 // components
 import { Nav } from './components/organisms/Nav'
-import { Modal } from './components/molecules/layout/Modal'
 
 // acess environment variables
 require('dotenv').config()
@@ -15,15 +13,9 @@ require('dotenv').config()
 /* -----------------------------------------------------JSX------------------------------------------------ */
 
 export default function App() {
-  const modal = useRef(null)
-
   useEffect(() => {
     getNewTokens()
   })
-
-  const onClickModal = () => {
-    modal.current.style.display = 'none'
-  }
 
   // Timer for refresh Token
   const refreshTokenTimer = () => {
@@ -36,24 +28,7 @@ export default function App() {
 
   return (
     <Router>
-      <>
-        <Nav />
-        <ModalDisclaimer
-          onClose={onClickModal}
-          ref={modal}
-          text={
-            "<p class='disclaimerTitle'>Disclaimer</p><br>Ce site a été réalisé à des fins pédagogiques dans le cadre du cursus Bachelor de l’école HETIC. Les contenus présentés n'ont pas fait l'objet d'une demande de droit d'utilisation. Ce site ne sera en aucun cas exploité à des fins commerciales et ne sera pas publié"
-          }
-        />
-      </>
+      <Nav />
     </Router>
   )
 }
-
-/* -----------------------------------------------------STYLE------------------------------------------------ */
-
-const ModalDisclaimer = styled(Modal)`
-  visibility: visible;
-  z-index: 101;
-  position: fixed;
-`
